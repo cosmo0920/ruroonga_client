0cb87d13378245a8ced0c7db8a1fd813fc22cbe9
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-01-10 00:08:11 +0900

    Add tiny result parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 extern crate hyper;
 extern crate url;
+extern crate json_flex;
 
 use std::io::Read;
 
@@ -7,6 +8,7 @@ use hyper::Client;
 use hyper::header::Connection;
 use hyper::header::ContentType;
 use url::form_urlencoded;
+use json_flex::{JFObject, Unwrap};
 
 pub struct HTTPRequest {
     client: Client,
@@ -76,5 +78,60 @@ impl<'a> CommandQuery<'a> {
 
     pub fn encode(&mut self) -> String {
         format!("/d/{}?{}", self.get_command(), self.make_query())
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct ResultParser {
+    result: Box<JFObject>
+}
+
+impl ResultParser {
+    pub fn new(json: String) -> ResultParser {
+        ResultParser{result: json_flex::decode(json)}
+    }
+
+    pub fn get_raw_object(&mut self) -> Box<JFObject> {
+        self.result.clone()
+    }
+
+    pub fn get_header(&mut self) -> JFObject {
+        self.result[0].clone()
+    }
+
+    pub fn status(&mut self) -> Option<&i64> {
+        self.result[0][0].into_i64().clone()
+    }
+
+    pub fn start_time(&mut self) -> Option<&f64> {
+        self.result[0][1].into_f64()
+    }
+
+    pub fn elapsed_time(&mut self) -> Option<&f64> {
+        self.result[0][2].into_f64()
+    }
+
+    #[inline]
+    fn matched_columns_num(&mut self) -> i64 {
+        self.result[1][0][0]
+            .unwrap_vec().clone()
+            .pop().unwrap()
+            .unwrap_i64().clone()
+    }
+
+    pub fn matched_columns(&mut self) -> Option<i64> {
+        match self.status() {
+            Some(&0) => Some(self.matched_columns_num()),
+            Some(_) => Some(-1),
+            None    => None
+        }
+    }
+
+    pub fn result(&mut self) -> Option<Vec<JFObject>> {
+        match self.status() {
+            Some(&0) => Some(vec![self.result[1][0].clone()]),
+            Some(_) => Some(vec![self.result[0][3].clone()]),
+            None    => None
+        }
     }
 }
